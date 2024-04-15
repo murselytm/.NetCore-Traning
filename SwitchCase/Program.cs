a2feec76d3b04c237e87db18879aaec7b7e3f56e
murselytm <mursel1yetim@gmail.com>
2024-04-15 03:10:41 +0300

    Switch case yapısı

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace SwitchCase{
+    class Program{
+         static void Main(string[]args){
+            int month = DateTime.Now.Month;
+
+            switch(month){
+                case 1:
+                Console.WriteLine("Ocak Ayındasınız");
+                break;
+                case 2:
+                Console.WriteLine("Şubat Ayındasınız");
+                break;
+                case 3:
+                Console.WriteLine("Mart Ayındasınız");
+                break;
+                case 4:
+                Console.WriteLine("Nisan Ayındasınız");
+                break;
+                default:
+                Console.WriteLine("Yanlış Veri girdiniz");
+                break;
+            }
+        }
+    }
+
+}
