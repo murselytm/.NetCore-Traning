862055f99b46dd9c4340e7108ee11037bf16e8e9
murselytm <mursel1yetim@gmail.com>
2024-04-15 03:01:28 +0300

    İf else yapısı

---
+++ IfElseCondition/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace IfElseCondition{
+    class Program{
+        static void Main(string[]args){
+            int time = DateTime.Now.Hour;
+            if(time>=6 && time<11 ){
+            Console.WriteLine("Günaydın");
+            }else if(time>=18){
+                Console.WriteLine("İyi Günler");
+            }
+            else{
+                Console.WriteLine("İyi geceler");
+            }
+
+            string sonuc = time<=18 ? "İyi günler" : "İyi geceler";
+
+            sonuc = time>=6 && time<11 ? "Günaydın" : time<= 18 ? "İyi Günler" : "İyi geceler";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
